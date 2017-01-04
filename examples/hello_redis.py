2419cbf11f07c44b370459382bdf3a69680126c1
Raphael Deem <raphael.deem@gmail.com>
2017-01-03 17:36:10 -0800

    Update hello_redis.py

--- examples/hello_redis.py
+++ examples/hello_redis.py
@@ -1,22 +1,22 @@
-import logging
-from sanic import Sanic
-from sanic.response import text
 from larry.backends import Redis
 from larry import Cache
+from time import sleep
 
 cache = Cache()
-app = Sanic()
 
-log = logging.getLogger()
-
-# We set the cache to start at 1
 # because the request object is not hashable
-@app.route("/<param>")
-@cache.cache(start=1, backend=Redis(host='localhost', port=6379), expires=10)
-def hello(request, param):
+@cache.cache(backend=Redis(host='localhost', port=6379), expires=3)
+def hello(param):
     s = [i**2 for i in range(int(param))]
-    log.info("hits: " + str(cache.funcs['hello'].hits))
-    log.info("misses: " + str(cache.funcs['hello'].misses))
+    print("hits: " + str(cache.funcs['hello'].hits))
+    print("misses: " + str(cache.funcs['hello'].misses))
     return text(s[-1])
+# miss
+hello(100000)
+# hit
+hello(100000)
 
-app.run(port=8000, debug=True)
+# cache expires
+sleep(3)
+# miss
+hello(100000)
